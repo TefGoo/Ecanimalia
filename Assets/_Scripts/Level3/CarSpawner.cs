448d81d4955f842f7326058c42fab5ff584e4f0e
Tefgoo <tefgoov@gmail.com>
2023-05-17 13:57:44 -0500

    Improved Duck Level
    Moved dino script to its correct folder, added first car sprite, new car spawn system working fine, with speed an interval.

--- Assets/_Scripts/Level3/CarSpawner.cs
+++ Assets/_Scripts/Level3/CarSpawner.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 
 public class CarSpawner : MonoBehaviour
 {
-    public GameObject carPrefab;
-    public float spawnInterval = 2f;
-    public float spawnSpeed = 1f;
+    public GameObject[] carPrefabs;
+    public float minSpawnInterval = 1f;
+    public float maxSpawnInterval = 3f;
+    public float minSpawnSpeed = 0.5f;
+    public float maxSpawnSpeed = 2f;
     public float spawnOffset = 0f;
     public float spawnDirection = 1f;
 
     private float timer = 0f;
+    private float spawnInterval;
+    private float spawnSpeed;
 
+
+    void Start()
+    {
+        CalculateSpawnInterval();
+        CalculateSpawnSpeed();
+    }
 
     void Update()
     {
@@ -19,12 +29,25 @@ public class CarSpawner : MonoBehaviour
         {
             timer -= spawnInterval;
             SpawnCar();
+            CalculateSpawnInterval();
+            CalculateSpawnSpeed();
         }
     }
 
     void SpawnCar()
     {
-        GameObject car = Instantiate(carPrefab, transform.position + new Vector3(spawnOffset * spawnDirection, 0f, 0f), Quaternion.identity);
+        int randomIndex = Random.Range(0, carPrefabs.Length);
+        GameObject car = Instantiate(carPrefabs[randomIndex], transform.position + new Vector3(spawnOffset * spawnDirection, 0f, 0f), Quaternion.identity);
         car.GetComponent<CarController>().moveSpeed = spawnSpeed * spawnDirection;
+    }
+
+    void CalculateSpawnInterval()
+    {
+        spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+    }
+
+    void CalculateSpawnSpeed()
+    {
+        spawnSpeed = Random.Range(minSpawnSpeed, maxSpawnSpeed);
     }
 }
