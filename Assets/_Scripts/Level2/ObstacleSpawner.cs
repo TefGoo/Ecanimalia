6b167be2be0128e5a1a1d01bac5db3815464b919
Tefgoo <tefgoov@gmail.com>
2023-08-17 19:55:13 -0500

    Jaguar Bullet
    Instead of contamination clouds, Jaguar will avoid bullets

--- Assets/_Scripts/Level2/ObstacleSpawner.cs
+++ Assets/_Scripts/Level2/ObstacleSpawner.cs
@@ -10,6 +10,9 @@ public class ObstacleSpawner : MonoBehaviour
     public float spawnInterval;
     public float minSpeed;
     public float maxSpeed;
+    public AudioClip floatingObstacleSpawnSound;
+
+    [SerializeField] private AudioSource soundManagerAudioSource; // Reference to the SoundManager's AudioSource
 
     private void Start()
     {
@@ -23,7 +26,6 @@ public class ObstacleSpawner : MonoBehaviour
             yield return new WaitForSeconds(spawnInterval);
             spawnInterval = Random.Range(2.3f, 3f);
 
-
             GameObject obstaclePrefab = Random.Range(0, 2) == 0 ? groundObstaclePrefab : floatingObstaclePrefab;
             float speed = Random.Range(minSpeed, maxSpeed);
 
@@ -31,9 +33,13 @@ public class ObstacleSpawner : MonoBehaviour
             float[] floatingHeights = new float[] { -4f, -3f, -2f };
             spawnPosition.y = obstaclePrefab == groundObstaclePrefab ? groundLevel : floatingHeights[Random.Range(0, floatingHeights.Length)];
 
-
             GameObject obstacle = Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity, obstacleParent);
             obstacle.GetComponent<ObstacleMovement>().Initialize(speed);
+
+            if (obstaclePrefab == floatingObstaclePrefab && soundManagerAudioSource != null && floatingObstacleSpawnSound != null)
+            {
+                soundManagerAudioSource.PlayOneShot(floatingObstacleSpawnSound);
+            }
         }
     }
 }
